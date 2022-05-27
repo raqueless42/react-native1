7acb3293b2763535f263f5e25204d3aa8c8ad542
Raquele <92033521+raqueless42@users.noreply.github.com>
2022-05-27 14:27:04 -0300

    Add files via upload

---
+++ src/telas/Cesta/componentes/Item.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, StyleSheet, FlatList} from "react-native";
+import Texto from "../../../componentes/Texto";
+import { Image } from "react-native";
+
+
+export default function Item ({item: {nome, imagem}}) {
+
+    return <View style={estilos.item}>
+    <Image source={imagem} style= {estilos.imagem}></Image>
+    <Texto style={estilos.nome}>{nome}</Texto>
+</View>
+
+}
+
+const estilos = StyleSheet.create ({
+    
+    item: {
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        borderBottomColor: "#ececec",
+        paddingVertical: 16,
+        alignItems: "center",
+       
+        marginHorizontal:16,
+    },
+    imagem: {
+        width: 46,
+        height: 46,
+    },
+    nome: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 11,
+        color: "#464646",
+        }
+})
